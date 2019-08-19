52ebf033be57b66a68dfe1ceaa354eed438774e7
ymmt1089 <ymmt1089@gmail.com>
2019-08-19 15:59:25 +0900

    [finish]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,11 @@
 class PostImage < ApplicationRecord
-
+	attachment :image
 	belongs_to :user
-	attachment :image#ここを追加(_idは含めない)
+	has_many :favorites,dependent: :destroy
 	has_many :post_comments,dependent: :destroy
-	
+	validates :shop_name, presence: true
+	validates :image, presence: true
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   root 'post_images#index'
 
-  resources :post_images, only: [:new, :create, :index, :show] do
-  	resource :post_comments, only: [:create, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+  	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 end
